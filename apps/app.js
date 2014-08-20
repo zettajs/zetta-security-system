b3dca7049b55c0f077abb8e3bb2732315db59213
Adam Magaluk <AdamMagaluK@gmail.com>
2014-08-20 15:52:23 -0400

    Updated dep to star for devices

--- apps/app.js
+++ apps/app.js
@@ -1,12 +1,15 @@
 module.exports = function(server) {
   var PIR = server.where({ type: 'pir' });
-  
-  server.observe(PIR, function(pir) {
+  var Buzzer = server.where({ type: 'buzzer' });
+
+  server.observe( [PIR, Buzzer], function(pir, buzzer) {
+    console.log('Found buzzer and pir')
     pir.on('motion', function() {
+      buzzer.call('turn-on', function() {});
       console.log('Motion Detected')
     });
-
     pir.on('no-motion', function() {
+      buzzer.call('turn-off', function() {});
       console.log('Motion is gone')
     });
   });
