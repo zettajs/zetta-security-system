06d6e726a76dd9c60ccc6e782362fd1e9448846d
Alan Languirand <=>
2014-10-02 13:59:44 -0400

    Fixed log message

--- server.js
+++ server.js
@@ -2,12 +2,9 @@ var zetta = require('zetta');
 var Buzzer = require('zetta-buzzer-bonescript-driver');
 var Microphone = require('zetta-microphone-bonescript-driver');
 
-var app = require('./apps/app');
-
 zetta()
   .use(Buzzer, 'P9_14')
   .use(Microphone, 'P9_36')
-  .use(app)
   .listen(1337, function(){
     console.log('Zetta is running at http://beaglebone.local:1337');
   });
